45b10395a3b722f0245c76fe67fd42ce6b060e24
SebastianFelgueras <sebastian.felgueras@gmail.com>
2021-10-01 17:08:37 -0300

    burbujas

--- templates.py
+++ templates.py
@@ -3,11 +3,6 @@ from jinja2 import Environment, FileSystemLoader
 from os import listdir
 renderizar = listdir("templates")
 renderizar.remove("root.html")
-"""with open("textos\\parrafos_index.txt",encoding="utf8") as archivo:
-    output = ""
-    for linea in archivo.read().splitlines():
-        output = f"{output}<p class=\"texto\">{linea}</p>\n"
-    open("textos\\parrafos_index_renderizado.txt",mode="w",encoding="utf8").write(output)"""
 
 templateLoader = FileSystemLoader(searchpath="./templates")
 templateEnv = Environment(loader=templateLoader)
