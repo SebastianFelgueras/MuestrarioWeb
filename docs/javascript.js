59684987afb36f10d529807c4d30be8f6b559c08
bylucho22 <bylucho22@gmail.com>
2021-10-05 15:22:59 -0300

    s

--- docs/javascript.js
+++ docs/javascript.js
@@ -1,12 +1,7 @@
-function fadeIn() {
+function fadeOut() {
     const titulo = document.getElementsByClassName("titulo");
     const subtitulo = document.getElementsByClassName('subtitulo');
     const boton = document.getElementsByClassName('btn');
-    titulo.style.animation = "slidein-left 2s, fadein 2s;";
-    subtitulo.style.animation = "slidein-left 2s, fadein 2s;";
-    boton.style.animation = "slidein-right 2s, fadein 2s;";
-}
-function fadeOut() {
     titulo.style.animationDirection = "reverse";
     subtitulo.style.animationDirection = "reverse";
     boton.style.animationDirection = "reverse";
